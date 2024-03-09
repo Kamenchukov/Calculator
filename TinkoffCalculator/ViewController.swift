d2ba5283996dd751e7c1408855d41e6124659342
Constantin <k.kamenchukov@gmail.com>
2024-03-09 17:42:32 +0100

    First commit
    Calculator

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Константин Каменчуков on 09.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        print(buttonText)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
